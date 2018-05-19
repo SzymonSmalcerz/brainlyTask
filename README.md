84ad0f67e65718d1511918858e23450ee9b178ca
Szymon Smalcerz <smalcerzszymonn@gmail.com>
2018-05-19 18:04:08 +0200

    first animation (wrong and positive answer) are done

--- README.md
+++ README.md
@@ -5,12 +5,8 @@ Internship challenge (Brainly 2018)
 ## App specification
 
 Create   a   single   page   flashcard   webapp   that   loads   data   from   external   URL   -  
-https://gist.githubusercontent.com/vergilius/6d869a7448e405cb52d782120b77b82c/raw/e75dc7
-c19b918a9f0f5684595899dba2e5ad4f43/history-flashcards.json.  
-Flashcards  
-should  
-be  
-presented   as   cards   (one   at   a   time)   with   questions   and   two   answers   (only   one   of   them   is   valid).   For  
+https://gist.githubusercontent.com/vergilius/6d869a7448e405cb52d782120b77b82c/raw/e75dc7c19b918a9f0f5684595899dba2e5ad4f43/history-flashcards.json.  
+Flashcards  should  be  presented   as   cards   (one   at   a   time)   with   questions   and   two   answers   (only   one   of   them   is   valid).   For  
 each   flashcard   user   should   be   able   to   choose   one   of   the   answers.   Choosing   invalid   answer  
 should   move   card   to   the   end   of   the   deck,   and   valid   answer   removes   card   from   the   deck.   After  
 adding correct answer to all questions users should get feedback on their performance.
--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-alert("here");
+let cards = document.getElementsByClassName("deck__card");
+let cardsArray = Array.from(cards);
+let wrongAnswer = document.getElementsByClassName("footer__answer--error")[0];
+let positiveAnswer = document.getElementsByClassName("footer__answer--success")[0];
+
+cardsArray.forEach(card => {
+  card.addEventListener('click', function(event){
+    //handleWrongAnswer(this);
+    handleCorrectAnswer(this);
+  })
+});
+
+
+let handleWrongAnswer = (card) => {
+  card.classList.add('deck__card--wrong');
+  wrongAnswer.style.display = "block";
+  setTimeout(() => {
+    let parentElement = card.parentElement;
+    parentElement.removeChild(card);
+    parentElement.insertBefore(card, parentElement.children[0]);
+    card.classList.remove('deck__card--wrong');
+    wrongAnswer.style.display = "none";
+  },1000)
+}
+
+let handleCorrectAnswer = (card) => {
+  card.classList.add('deck__card--positive');
+  positiveAnswer.style.display = "block";
+  setTimeout(() => {
+    let parentElement = card.parentElement;
+    parentElement.removeChild(card);
+    delete card;
+    positiveAnswer.style.display = "none";
+  },1000)
+}
